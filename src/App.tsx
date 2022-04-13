246cdef98d94c7129539631ccb19f89bed8054e1
myungsangBaek <bms6168@naver.com>
2022-04-13 09:42:53 +0900

    [FEATURE] Implement sendbird provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { App as SendBirdApp } from "sendbird-uikit";
+import { SendBirdProvider, withSendBird, ChannelList } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
 
 import "./App.css";
 
-function App() {
+export interface IProps {
+  id: string;
+}
+
+function App({ id }: IProps) {
   return (
     <div className="App">
-      <SendBirdApp
-        appId="C2B2D423-1D82-4033-9766-C6445C320F17"
-        userId="bms1643"
-      />
+      <SendBirdApp appId="C2B2D423-1D82-4033-9766-C6445C320F17" userId={id}>
+        <SendBirdProvider
+          appId="C2B2D423-1D82-4033-9766-C6445C320F17"
+          userId={id}
+        ></SendBirdProvider>
+      </SendBirdApp>
     </div>
   );
 }
