50fef351723ffd7ef662606106add765178ebbda
myungsangBaek <bms6168@naver.com>
2022-04-13 10:54:17 +0900

    [FEATURE] Get properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { App as SendBirdApp } from "sendbird-uikit";
-import { SendBirdProvider, withSendBird, ChannelList } from "sendbird-uikit";
+
 import "sendbird-uikit/dist/index.css";
 
 import "./App.css";
@@ -12,14 +12,18 @@ export interface IProps {
 function App({ id }: IProps) {
   return (
     <div className="App">
-      <SendBirdApp appId="C2B2D423-1D82-4033-9766-C6445C320F17" userId={id}>
-        <SendBirdProvider
-          appId="C2B2D423-1D82-4033-9766-C6445C320F17"
-          userId={id}
-        ></SendBirdProvider>
-      </SendBirdApp>
+      <SendBirdApp
+        appId={"C2B2D423-1D82-4033-9766-C6445C320F17"}
+        userId={id}
+        nickname={"han"}
+        profileUrl={
+          "https://dododot.net/_next/image?url=https%3A%2F%2Fdododot-website.s3.ap-northeast-2.amazonaws.com%2Fmember%2Fthumbnail_myungsang.png&w=1920&q=75"
+        }
+        theme={"dark"}
+        // userListQuery={userListQuery}
+      ></SendBirdApp>
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
