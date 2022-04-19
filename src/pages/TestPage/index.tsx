36c918f653145a6eb71a006eeef8d5319c3c0f76
myungsangBaek <bms6168@naver.com>
2022-04-19 14:06:54 +0900

    [FEATURE] Generate use typescript input

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Greetings from "../../components/Greetings";
+import MyForm from "../../components/MyForm";
 
 const TestPage: React.FC = () => {
   const [count, setCount] = useState<number>(0);
@@ -11,12 +12,16 @@ const TestPage: React.FC = () => {
   const onIncrease = () => setCount(count + 1);
   const onDncrease = () => setCount(count - 1);
 
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Greetings name="Han" onClick={onClick} />
       <button onClick={onIncrease}>{"+1"}</button>
       <button onClick={onDncrease}>{"-1"}</button>
       <div>{count}</div>
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 };
